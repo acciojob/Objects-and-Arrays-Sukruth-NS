678f97fe1aaa41172202cd65aeb85aa51764444e
Sukruth Surendra N S <113716919+Sukruth-NS@users.noreply.github.com>
2023-07-19 12:21:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,8 +7,18 @@ const person = {
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
-window.players = players;
-window.person = person;
-window.team = team;
-window.team1 = team1;
-window.cap1 = cap1;
+// window.players = players;
+// window.person = person;
+// window.team = team;
+// window.team1 = team1;
+// window.cap1 = cap1;
+
+const team = players;
+
+const team1 = players.slice();// Using slice() to create a shallow copy
+
+const cap1 = Object.assign({}, person);// Using Object.assign() to create a shallow copy
+
+console.log(team);   
+console.log(team1);  
+console.log(cap1);  
